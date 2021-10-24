f074e2a4e6cc4416c8a762f87c38efe739977491
scsk-pbl <92980177+scsk-pbl@users.noreply.github.com>
2021-10-25 03:17:36 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install -y docker-ce
+
 git clone https://github.com/toppers/hakoniwa-single_robot.git
 
 cd hakoniwa-single_robot
